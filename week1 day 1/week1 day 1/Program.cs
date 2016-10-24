8582bbf8fdf1c667de0dd1bab77327e0f4a9d7f1
ashfill <ashtonfillery@gmail.com>
2016-10-24 06:01:15 -0700

    yes

---
+++ week1 day 1/week1 day 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1_day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("hello world");
+            int myint = 1492;
+            Console.WriteLine(myint);
+            string time = DateTime.Now.ToShortTimeString();
+            string date = DateTime.Now.ToShortDateString();
+            Console.WriteLine(time);
+            Console.WriteLine(date);
+            string single = ("x");
+            Console.WriteLine(single);
+            decimal value = 5.45m;
+            Console.WriteLine(value.ToString("c2"));
+            string newstring = myint.ToString("Columbus sail the world in 1492");
+            Console.WriteLine(newstring);
+            Console.WriteLine(value.ToString("I spent $5.45 on a cheeseburger"));
+            Console.WriteLine(single.ToString("x marks the spot"));
+            Console.ReadLine();
+        }
+
+    }
+}
